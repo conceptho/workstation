efe36457c98db11b62f19dc91990638390a09437
Andre Metzen <andressantos@gmail.com>
2015-06-03 17:29:29 -0300

    Upstart and finishing setup

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-
 sudo apt-get install dnsmasq git gitg openjdk-7-jre
 echo "address=/localhost/127.0.0.1" | sudo tee /etc/dnsmasq.d/localhost.conf > /dev/null
 sudo service dnsmasq restart 
@@ -9,10 +8,16 @@ sudo wget -qO- https://get.docker.com/ | sh
 
 sudo service docker start
 
-
 sudo curl -L https://github.com/docker/compose/releases/download/1.2.0/docker-compose-`uname -s`-`uname -m` | sudo tee /usr/local/bin/docker-compose > /dev/null
 sudo chmod +x /usr/local/bin/docker-compose
 
 wget http://download.netbeans.org/netbeans/8.0.2/final/bundles/netbeans-8.0.2-php-linux.sh
 chmod +x netbeans-8.0.2-php-linux.sh
 ./netbeans-8.0.2-php-linux.sh
+
+mkdir ~/.conceptho
+
+cp * ~/.conceptho
+ln -s ~/upstart.conf ~/.config/upstart/workstation.conf
+sed -i "s@{HOME}@${HOME}@g" ~/.config/upstart/workstation.conf
+sudo start workstation

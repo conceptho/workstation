a665d04334f41d6b4c8e2e7d623c75fa25375829
Andre Metzen <andressantos@gmail.com>
2015-06-03 17:06:53 -0300

    Initial

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+
+sudo apt-get install dnsmasq git gitg openjdk-7-jre
+echo "address=/localhost/127.0.0.1" | sudo tee /etc/dnsmasq.d/localhost.conf > /dev/null
+sudo service dnsmasq restart 
+
+sudo wget -qO- https://get.docker.com/ | sh
+
+sudo service docker start
+
+
+sudo curl -L https://github.com/docker/compose/releases/download/1.2.0/docker-compose-`uname -s`-`uname -m` | sudo tee /usr/local/bin/docker-compose > /dev/null
+sudo chmod +x /usr/local/bin/docker-compose
+
+wget http://download.netbeans.org/netbeans/8.0.2/final/bundles/netbeans-8.0.2-php-linux.sh
+chmod +x netbeans-8.0.2-php-linux.sh
+./netbeans-8.0.2-php-linux.sh

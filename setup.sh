6c8907631ec36f1f04c404b410b5972d6152057f
André Bazaglia <andre@bazaglia.com>
2016-10-03 17:12:15 -0300

    General improvements

--- setup.sh
+++ setup.sh
@@ -1,21 +1,56 @@
 #!/bin/bash
 
-# Docker
+# global vars.
+UBUNTU_VER=$(lsb_release -sr)
+PHPSTORM_VER=2016.2.1
+DOCKERCOMPOSE_VERSION=1.8.0
 
+# apt-get init.
 sudo apt-get update --fix-missing
-sudo apt-get install -y curl dnsmasq git gitg openjdk-7-jre
+sudo apt-get install -y curl dnsmasq git gitg openjdk-8-jre apt-transport-https ca-certificates
+
+# docker-apt-key
+sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+
+# dnsmasq
 echo "address=/localhost/127.0.0.1" | sudo tee /etc/dnsmasq.d/localhost.conf > /dev/null
 sudo service dnsmasq restart
 
-sudo wget -qO- https://get.docker.com/ | sh
+# add docker repo.
+case $UBUNTU_VER in
+   "16.04") DOCKER_REPO="deb https://apt.dockerproject.org/repo ubuntu-xenial main";;
+   "15.10") DOCKER_REPO="deb https://apt.dockerproject.org/repo ubuntu-xenial main";;
+   "14.04") DOCKER_REPO="deb https://apt.dockerproject.org/repo ubuntu-trusty main";;
+   "12.04") DOCKER_REPO="deb https://apt.dockerproject.org/repo ubuntu-precise main";;
+   *) echo "Sorry, docker repo unavailable for your Ubuntu version.";;
+esac
+
+sudo echo $DOCKER_REPO > sudo /etc/apt/sources.list.d/docker.list
+sudo apt-get update
+sudo apt-get install linux-image-extra-$(uname -r) linux-image-extra-virtual docker-engine
 
 sudo service docker start
 
-sudo curl -L https://github.com/docker/compose/releases/download/1.7.1/docker-compose-`uname -s`-`uname -m` | sudo tee /usr/local/bin/docker-compose > /dev/null
+# docker group
+sudo groupadd docker
+sudo usermod -aG docker $USER
+
+sudo curl -L https://github.com/docker/compose/releases/download/${DOCKERCOMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` | sudo tee /usr/local/bin/docker-compose > /dev/null
 sudo chmod +x /usr/local/bin/docker-compose
 
-# NetBeans
+# PhpStorm
+read -p "Install PhpStorm? " -n 1 -r
+echo    # move to new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  wget https://download.jetbrains.com/webide/PhpStorm-${PHPSTORM_VER}.tar.gz
+  PHPSTORM_DIR=`tar -zxvf PhpStorm-${PHPSTORM_VER}.tar.gz --directory ~/Documents| head -1 | cut -f1 -d"/"`
+  rm -rf ~/Documents/${PHPSTORM_DIR}
+  tar -zxvf PhpStorm-${PHPSTORM_DIR}.tar.gz --directory ~/Documents
+  ~/Documents/${PHPSTORM_VER}/bin/phpstorm.sh &
+ fi
 
+# NetBeans
 read -p "Install NetBeans? " -n 1 -r
 echo 	# move to new line
 if [[ $REPLY =~ ^[Yy]$ ]]
@@ -30,9 +65,7 @@ fi
 mkdir ~/.conceptho
 cp -R * ~/.conceptho
 
-read -p "Ubuntu >= 15.04? " -n 1 -r
-echo    # move to a new line
-if [[ $REPLY =~ ^[Yy]$ ]]
+if [[ $UBUNTU_VER >= 15.04 | bc ]]
 then
   sudo systemctl enable docker
   sudo ln -s ~/.conceptho/workstation.service /etc/systemd/system/workstation.service

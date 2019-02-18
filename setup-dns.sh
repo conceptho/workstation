6d2a62e04ec172577aeab2ef805b5210993c9cdd
Rômulo Guimarães <romulo1984@gmail.com>
2019-02-18 19:04:11 -0300

    Portainer added

---
+++ setup-dns.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# apt-get init.
+sudo apt-get update --fix-missing
+sudo apt-get install -y curl dnsmasq ca-certificates
+
+# dnsmasq
+echo "address=/localhost/127.0.0.1" | sudo tee /etc/dnsmasq.d/localhost.conf > /dev/null
+echo "address=/docker/127.0.0.1" | sudo tee /etc/dnsmasq.d/docker.conf > /dev/null
+sudo service dnsmasq restart
+
+# docker group
+sudo groupadd docker
+sudo usermod -aG docker $USER
+
+# Services
+mkdir ~/.conceptho
+cp -R * ~/.conceptho
+
+sudo systemctl enable docker
+sudo ln -s ~/.conceptho/workstation.service /etc/systemd/system/workstation.service
+sudo sed -i "s@{HOME}@${HOME}@g" /etc/systemd/system/workstation.service
+sudo service workstation start
